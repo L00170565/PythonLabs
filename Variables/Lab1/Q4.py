db39c0abc25b5254da882b4a544fba435253493f
L00170565 <L00170565@student.lyit.ie>
2021-11-14 19:09:02 -0800

    Question 4 ready

---
+++ Variables/Lab1/Q4.py
@@ -0,0 +1,18 @@
+# ------------------------------------------
+# Project = PythonLabs
+# FileName = Q4.py
+#
+# (C) 2021 Panagiotis Drakos, L00170565
+#
+# Lab Description: Carry out the following calculations.
+# Why are these examples important?
+# ------------------------------------------
+
+print(6 + 8 / 2 - 1)
+print((1 + 1 - 1 + 2 )/ 2)
+print((1.1 + 5) / 2)
+print(9.2 / 3)
+print(9.2 % 3)
+print(1 * (2 + 3) * 5 % 4)
+print(((1 + 8) % 3 )* (2 - 9))
+print(6 + (24 % (16 - 11)))
