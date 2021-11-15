3ec8c7da82bd5d5af84055c601a5167c2bc8054c
L00170565 <L00170565@student.lyit.ie>
2021-11-14 17:46:46 -0800

    Question 2 ready

--- Variables/Lab1/Question2.py
+++ Variables/Lab1/Question2.py
@@ -1,16 +1,22 @@
+# ------------------------------------------
+# Project = PythonLabs
+# FileName = Question2.py
+#
+# (C) 2021 Panagiotis Drakos, L00170565
+# ------------------------------------------
 
-x = " < "
-y = " > "
-z = "Title"
-
-print(50*'^')
-print("{0} {1:^95} ".format("<",">"))
-print("{0} {1:^95} ".format("<",">"))
-print("{0} {1:^95} ".format("<",">"))
-
-print("{0} {1:^95} ".format("<",">"))
-print("{0} {1:^95} ".format("<",">"))
-print("{0} {1:^95} ".format("<",">"))
-print(50*'v')
+print(50*"^")
+print("{0} {1:^95s} ".format("<",">"))
+print("{0} {1:^95s} ".format("<",">"))
+print("{0} {1:^95s} ".format("<",">"))
+print("< {:^46s} >".format("Title"))
+print("{0} {1:^95s} ".format("<",">"))
+print("{0} {1:^95s} ".format("<",">"))
+print("<    {0:}    {1:<38s} >".format("A", "Option a" ))
+print("<    {0:}    {1:<38s} >".format("B", "Option b" ))
+print("<    {0:}    {1:<38s} >".format("C", "Option c" ))
+print("{0} {1:^95s} ".format("<",">"))
+print("{0} {1:^95s} ".format("<",">"))
+print(50*"v")
 
 
