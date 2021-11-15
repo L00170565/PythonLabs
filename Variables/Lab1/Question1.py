4f43ebcd992883aefa26baec0d43b2ab99c7d378
L00170565 <L00170565@student.lyit.ie>
2021-11-14 17:54:49 -0800

    Question 1 ready

---
+++ Variables/Lab1/Question1.py
@@ -0,0 +1,25 @@
+# FileName = Question1.py
+#
+# (C) 2021 Panagiotis Drakos, L00170565
+#
+# Lab Description: Test the following statements in PyCharm
+# ------------------------------------------
+
+print("\t"*2+"Title")
+print("Hello")
+print('Hello World')
+a=5
+b=12.3456789
+c=a+b
+d="A number "+str(b) # can only concatenate str (not "float") to str
+e=a+ \
+ b+ \
+ c
+f=2
+g=a/f
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
