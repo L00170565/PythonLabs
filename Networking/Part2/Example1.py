bfd729b603a2ea6db9d4ca987f30ad6dba462591
L00170565 <L00170565@student.lyit.ie>
2021-11-18 09:44:59 -0800

    comments

---
+++ Networking/Part2/Example1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# ------------------------------------------
+# Project = PythonLabs
+# FileName = Lab8/Q1.py
+#
+# (C) 2021 Panagiotis Drakos, L00170565
+# Networking Lab exercises and Examples
+# Description: This example shows how to read the contents of a page using urllib. The contents of the page are
+# large and should be parsed further but this serves as a basic example.
+# ------------------------------------------
