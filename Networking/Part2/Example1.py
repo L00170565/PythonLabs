794de43f50fa7a42e3a04f3633045993285d16ba
L00170565 <L00170565@student.lyit.ie>
2021-11-18 10:04:59 -0800

    comments

--- Networking/Part2/Example1.py
+++ Networking/Part2/Example1.py
@@ -2,10 +2,26 @@
 
 # ------------------------------------------
 # Project = PythonLabs
-# FileName = Lab8/Q1.py
+# FileName = Lab8-Part2/Q1.py
 #
 # (C) 2021 Panagiotis Drakos, L00170565
 # Networking Lab exercises and Examples
-# Description: This example shows how to read the contents of a page using urllib. The contents of the page are
-# large and should be parsed further but this serves as a basic example.
+# Description: This example shows how a list of ip addresses can be validated by separating out the parts of the
+# string that represent each octet. We have looked at another way of validating ip addresses in
+# another handout via regular expressions.
 # ------------------------------------------
+
+# Check reachability of the server
+import sys
+
+# Checking each of the 4 octets that make up a valid ip address
+def ip_address_valid(list):
+    for ip in list:
+        ip = ip.rstrip
+
+
+
+
+
+
+
