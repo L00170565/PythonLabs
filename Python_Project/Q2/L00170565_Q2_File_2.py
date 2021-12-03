b08b46c85615e8aea6c4728fb61e9007ddb18d1d
L00170565 <L00170565@student.lyit.ie>
2021-12-03 08:54:16 -0800

    check headers of the page

--- Python_Project/Q2/L00170565_Q2_File_2.py
+++ Python_Project/Q2/L00170565_Q2_File_2.py
@@ -18,12 +18,15 @@ def read_Page_Contents():
     """ scrape web page contents"""
     print("Contents of Page")
 
-    html = urlopen("https://en.wikipedia.org/wiki/Main_Page")
+    #url = urlopen("https://www.lyit.ie/")
+    url = urlopen("http://192.168.209.138")
+    soup = BeautifulSoup(url, "html.parser") # Python html parser
+   # soup = BeautifulSoup(url, features="html5lib")
 
-    soup = BeautifulSoup(html, "html.parser") # Python html parser
     titles = soup.find_all(['h1', 'h2','h3','h4','h5','h6']) # Headers to read
 
     print('List all the header tags:', *titles, sep='\n\n') # Print all the headers and separate them with newline
+
 
 if __name__ == "__main__":
     read_Page_Contents()
