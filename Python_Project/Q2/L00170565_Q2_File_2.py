9a347aa4a8ec36108222a75ffb0fa0c6c20c1030
L00170565 <L00170565@student.lyit.ie>
2021-12-02 18:27:05 -0800

    Q1 and Q2 added

---
+++ Python_Project/Q2/L00170565_Q2_File_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# ------------------------------------------
+# Project = PythonLabs
+# FileName = Lab8-Part2/Q2.py
+#
+# (C) 2021 Panagiotis Drakos, L00170565
+# Networking Lab exercises and Examples
+# Description: This example shows how a list of ip addresses for servers can be tested for connectivity.
+# ------------------------------------------
+
+
+
+
+
+
+
+
+
+
