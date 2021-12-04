8eea88c00315b179923fc0415b7af7e072009c62
L00170565 <L00170565@student.lyit.ie>
2021-12-04 02:36:30 -0800

    scrap info latest

--- Python_Project/Q2/L00170565_Q2_File_2.py
+++ Python_Project/Q2/L00170565_Q2_File_2.py
@@ -11,45 +11,48 @@
 # Import the libraries
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 # Step 1: Define User-Agent and make an HTTP response to the specific URL
-headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+# noinspection SpellCheckingInspection
+headers = {
+    'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
 
 # Step 3: Create an empty list to append with the dictionary
-cart = [] # A new empty list
+cart_list = []  # A new empty list
 
-def read_Page_Contents():
+
+def read_page_contents():
+    """
+    ...
+    """
     url = 'https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2499334.m570.l1311&_nkw=dewalt+impact+driver&_sacat=0'
-    response = requests.get(url, headers = headers) # Make a get response to fetch the content
-    soup = BeautifulSoup(response.text, 'html.parser') # Use Python HTML parser
+    response = requests.get(url, headers=headers)  # Make a get response to fetch the content
+    soup = BeautifulSoup(response.text, 'html.parser')  # Use Python HTML parser
 
-    #print(soup.title.text)
-    questions = soup.find_all('div', {'class': 's-item__info clearfix'}) # Find all tags with that instance
+    # print(soup.title.text)
+    questions = soup.find_all('div', {'class': 's-item__info clearfix'})  # Find all tags with that instance
 
-    #print(len(questions))
+    # print(len(questions))
 
     # Step 2: Transform it to a dictionary to use it to append a list later
     for item in questions:
         question = {
-        'title ' : item.find('a', {'class' : 's-item__link'}).text, # Find as text all the tags with that instance
-        'buy' : item.find('span', {'class': 's-item__purchase-options-with-icon'}), #Find item that is classified as buy it now
-        'watchers' : item.find('span', {'class': 's-item__dynamic s-item__additionalItemHotness'}), #Find with tag for watchers
-        'rating' : item.find('span', {'class': 's-item__reviews-count'}),
-        'country' : item.find('span', {'class': 's-item__location s-item__itemLocation'})
+            'title ': item.find('a', {'class': 's-item__link'}).text,  # Find as text all the tags with that instance
+            'buy': item.find('span', {'class': 's-item__purchase-options-with-icon'}),
+            # Find item that is classified as buy it now
+            'watchers': item.find('span', {'class': 's-item__dynamic s-item__additionalItemHotness'}),
+            # Find item with tag for watchers
+            'rating': item.find('span', {'class': 's-item__reviews-count'}),
+            'country': item.find('span', {'class': 's-item__location s-item__itemLocation'})
         }
-        cart.append(question)
-    print(*cart, sep = '\n')
+        cart_list.append(question)
+    # print(len(cart))
+    print(*cart_list, sep='\n')
+
 
 if __name__ == "__main__":
-    read_Page_Contents()
-
-
-
-
-
-
-
-
-
-
+    read_page_contents()
+    # df = pd.DataFrame(cart_list)  # Need to update this
+    # df.to_excel('test.xlsx')
