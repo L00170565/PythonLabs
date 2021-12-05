2828d48da98431e1263b8aa06bae0b1219343b94
Panagiotis Drakos <L00170565@student.lyit.ie>
2021-12-05 04:57:54 -0800

    ssh connection1

--- Python_Project/Q3/L00170565_Q3_File_1.py
+++ Python_Project/Q3/L00170565_Q3_File_1.py
@@ -9,30 +9,27 @@
 # Copyright = (C) 2021 Panagiotis Drakos
 # ------------------------------------------
 """ Description: We have first imported the Paramiko library SSHClient in order to be able to initiate
-an SSH session with remote host. As a next step, variables were created to include the required
-information for the SSH session. For an already known host, host system keys can be loaded,
+an SSH session with remote host. For an already known host, host system keys can be loaded,
 but in order to be more secure, Paramiko's AutoAddPolicy has been used, and instead of loading the
 host keys from the system itself, unknown host keys are used. With that second part configured,
 a try-catch block is used and execute the instructions for connection. As an extra level of security
-in the script we can ask from the user to type required credentials for the SSH session"""
+in the script we can ask from the user to type required credentials for the SSH session by importing
+library getpass. Script can be run through terminal"""
 
 from paramiko.client import SSHClient, AutoAddPolicy
 import getpass
 
 
 def ssh_connection(ip):
-    user_name = input("Username: ")
-    user_pass = getpass.getpass(prompt='Password: ', stream=None)
-    remote_ip = input("Host: ")
-    remote_port = int(input("Port: "))
 
     client = SSHClient()
     # client.load_system_host_keys() # For already known hosts
     client.set_missing_host_key_policy(AutoAddPolicy())  # Use unknown host keys for the script
     try:
-        client.connect(remote_ip, port=remote_port,
-                       username=user_name,
-                       password=user_pass,
+        # Get user's credentials to initiate the SSH connection
+        client.connect(input("Host: "), port=int(input("Port: ")),
+                       username=input("Username: "),
+                       password=getpass.getpass(prompt='Password: ', stream=None),
                        look_for_keys=False)
         print("Connected Successfully...")
         client.close()  # close the session
