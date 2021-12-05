73888e5c9010c2cc4cf1a6b4bd9008b29d044a1d
Panagiotis Drakos <L00170565@student.lyit.ie>
2021-12-05 06:33:39 -0800

    ssh connection and execute command

---
+++ Python_Project/Q4/L00170565_Q4_File_1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# ------------------------------------------
+# File Name = L00170565_Q4_File_2.py
+# Project = Python Project
+#
+# Author = Panagiotis Drakos, L00170565
+# Date = 03/12/21
+# Copyright = (C) 2021 Panagiotis Drakos
+# ------------------------------------------
+
+from paramiko.client import SSHClient, AutoAddPolicy
+import getpass
+import time
+import re
+import paramiko
+
+
+def ssh_connection(ip):
+    client = SSHClient()
+    # client.load_system_host_keys() # For already known hosts
+    client.set_missing_host_key_policy(AutoAddPolicy())  # Use unknown host keys for the script
+    try:
+        # Get user's credentials to initiate the SSH connection
+        client.connect(input("Host: "), port=int(input("Port: ")),
+                       username=input("Username: "),
+                       password=getpass.getpass(prompt='Password: ', stream=None),
+                       look_for_keys=False)
+        print("Connected Successfully...")
+        connection = client.invoke_shell()
+        connection.send("ifconfig > ifconfig.txt\n")
+        time.sleep(2)
+
+        vm_output = connection.recv(65535)
+        if re.search(b"% Invalid input", vm_output):
+            print("There was an error on vm {}".format(ip))
+        else:
+            print("Command successfully executed on {}".format(ip))
+        client.close()  # close the session
+    except paramiko.AuthenticationException:
+        print("Failed to connect")
+    # finally:
+    #     client.close()
+
+
+ssh_connection("192.168.209.138")
+
+
+
+
+
+
+
+
+
+
+
