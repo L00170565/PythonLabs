e3f14435a8c69653fd099645bc7805abf1dc42d9
Panagiotis Drakos <L00170565@student.lyit.ie>
2021-12-05 01:58:53 -0800

    ssh connection1

--- Python_Project/Q3/L00170565_Q3_File_1.py
+++ Python_Project/Q3/L00170565_Q3_File_1.py
@@ -15,7 +15,7 @@ import time
 import re
 
 
-def ssh_connection(ip=None):
+def ssh_connection(ip):
     try:
         username = 'dev'
         password = 'dev123!'
@@ -26,7 +26,20 @@ def ssh_connection(ip=None):
         session.connect(ip.rstrip("\n"), username=username, password=password)
         connection = session.invoke_shell()
         connection.send("ls > dir_contents.txt\n")
+        time.sleep(1)
 
+        vm_output = connection.recv(65535)
+        if re.search(b"% Invalid input", vm_output):
+            print("There was an error on vm {}".format(ip))
+        else:
+            print("Command successfully executed on vm {}".format(ip))
+        session.close()
+    except paramiko.AuthenticationException:
+        print("Authentication Error")
+
+
+if __name__ == "__main__":
+    ssh_connection("192.168.209.139")
 
 
 
