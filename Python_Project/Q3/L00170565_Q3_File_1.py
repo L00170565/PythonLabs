d06650069d473248535444cc8b89858ba22212d2
Panagiotis Drakos <L00170565@student.lyit.ie>
2021-12-05 04:38:57 -0800

    ssh connection1

--- Python_Project/Q3/L00170565_Q3_File_1.py
+++ Python_Project/Q3/L00170565_Q3_File_1.py
@@ -13,16 +13,18 @@ an SSH session with remote host. As a next step, variables were created to inclu
 information for the SSH session. For an already known host, host system keys can be loaded,
 but in order to be more secure, Paramiko's AutoAddPolicy has been used, and instead of loading the
 host keys from the system itself, unknown host keys are used. With that second part configured,
-a try-catch block is used and execute the instructions for connection"""
+a try-catch block is used and execute the instructions for connection. As an extra level of security
+in the script we can ask from the user to type required credentials for the SSH session"""
 
 from paramiko.client import SSHClient, AutoAddPolicy
+import getpass
 
 
 def ssh_connection(ip):
-    user_name = "dev"  # provide username
-    user_pass = "dev123!"  # provide user password
-    remote_ip = "192.168.209.138"  # provide remote host to connect with ssh
-    remote_port = 22
+    user_name = input("Username: ")
+    user_pass = getpass.getpass(prompt='Password: ', stream=None)
+    remote_ip = input("Host: ")
+    remote_port = int(input("Port: "))
 
     client = SSHClient()
     # client.load_system_host_keys() # For already known hosts
