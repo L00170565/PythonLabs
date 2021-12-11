3f4b58dd9a238d5bfcaf0da105d2683d8a5cfe1e
L00170565 <L00170565@student.lyit.ie>
2021-12-11 14:09:49 -0800

    edit docstring

--- Variables/Lab1/Q1.py
+++ Variables/Lab1/Q1.py
@@ -2,25 +2,25 @@
 
 # ------------------------------------------
 # Project = PythonLabs
-# FileName = Q1.py
+# FileName = Lab1/Q1.py
 #
 # (C) 2021 Panagiotis Drakos, L00170565
-#
-# Lab Description: Test the following statements in PyCharm
 # ------------------------------------------
 
-print("\t"*2+"Title")
+"""Lab Description: Test the following statements in PyCharm"""
+
+print("\t" * 2 + "Title")
 print("Hello")
 print('Hello World')
-a=5
-b=12.3456789
-c=a+b
-d="A number "+str(b) # can only concatenate str (not "float") to str
-e=a+ \
- b+ \
- c
-f=2
-g=a/f
+a = 5
+b = 12.3456789
+c = a + b
+d = "A number " + str(b)  # can only concatenate str (not "float") to str
+e = a + \
+    b + \
+    c
+f = 2
+g = a / f
 print(a)
 print(b)
 print(c)
