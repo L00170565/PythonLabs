ccd14aa5d91c9d0b6d10840d6207d79bed3639b8
L00170565 <L00170565@student.lyit.ie>
2021-12-11 14:21:41 -0800

    edit docstring

--- Variables/Lab2/Q3.py
+++ Variables/Lab2/Q3.py
@@ -5,22 +5,12 @@
 # FileName = Lab2/Q3.py
 #
 # (C) 2021 Panagiotis Drakos, L00170565
-#
 # ------------------------------------------
 
-# Lab Description: Given the variable suffix_no with a value of 1122. Build an LYIT student number. The number should
-# begin with an L and be followed by 8 digits. Where the suffix_no is less than 8 the string should be
-# padded with zero’s to a final size of 8. Use only string methods and variables to carry out this task. Do
-# not use loops or selection structures. Modify your answer to take in the suffix_no from the user.
+"""Lab Description: Given the variable suffix_no with a value of 1122. Build an LYIT student number. The number should
+begin with an L and be followed by 8 digits. Where the suffix_no is less than 8 the string should be
+padded with zero’s to a final size of 8. Use only string methods and variables to carry out this task. Do
+not use loops or selection structures. Modify your answer to take in the suffix_no from the user."""
 
-suffix_no = int(input("Please provide your LYIT Num:")) # Get string from user and convert it to int
+suffix_no = int(input("Please provide your LYIT Num:"))  # Get string from user and convert it to int
 print("L{:0>8}".format(suffix_no))
-
-
-
-
-
-
-
-
-
